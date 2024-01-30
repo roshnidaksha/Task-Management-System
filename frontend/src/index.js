b60e4f65da9fa517683bfcabeae2720bd128b4c0
Roshni Daksha <groshnidaksha2@yahoo.com>
2024-01-30 22:53:17 +0800

    created browserRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import App from './App';
+import loginPage from "./pages/loginPage.tsx";
+import signupPage from "./pages/signupPage.tsx";
+import mainPage from "./pages/mainPage.tsx";
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <mainPage />,
+      },
+      {
+        path: "/login",
+        element: <loginPage />,
+      },
+      {
+        path: "/signup",
+        element: <signupPage />,
+      },
+    ],
+  },
+]);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ frontend/src/pages/loginPage.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { useContext, useState } from "react";
+
+function loginPage(props) {
+    
+}
