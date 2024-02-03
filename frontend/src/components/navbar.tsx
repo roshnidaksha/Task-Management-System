4429b76dd508e75f7370d4be8b59abbf06283329
Roshni Daksha <groshnidaksha2@yahoo.com>
2024-02-03 14:41:03 +0800

    Interactive Navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,20 +1,69 @@
-import React from 'react';
-import {  Link } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-const navbar = () => {
-  return (
-  <div>
-    <li>
-      <Link to="/">Main</Link>
-    </li>
-    <li>
-      <Link to="/login">Login</Link>
-    </li>
-    <li>
-      <Link to="/signup">Signup</Link>
-    </li>
-  </div>
-  );
+const Navbar = () => {
+    const navigate = useNavigate();
+    function handleLogin() {
+        navigate("/login");
+    }
+
+    function handleSignup() {
+        navigate("/signup");
+    }
+
+    return (
+        <AppBar position="static" className={"px-0 md: px-4"}>
+            <Toolbar>
+                <div>
+                    <Link to={"/"} className={"no-underline"}>
+                        <Typography variant="h6" className={"text-White"}>
+                            Innovation Hub
+                        </Typography>
+                    </Link>
+                </div>
+
+                <div className={"flex-grow"} />
+
+                <div className={"mr-4"}>
+                    <Button color="inherit" onClick={handleSignup}>
+                        <Typography className={"text-white"}>
+                            Sign Up
+                        </Typography>
+                    </Button>
+                </div>
+
+                <div className={"mr-2 flex min-w-[30px] items-center gap-2 md:mr-6"}>
+                    <div className={"hidden md: block"}>
+                        <Typography className={"text-white"}>
+                            Welcome
+                        </Typography>
+                    </div>
+                </div>
+
+                <Button color="inherit" onClick={handleLogin}>
+                    <Typography className={"text-white"}>
+                        Login
+                    </Typography>
+                </Button>
+            </Toolbar>
+        </AppBar>
+    );
 }
 
-export default navbar;
+<div>
+<li>
+  <Link to="/">Main</Link>
+</li>
+<li>
+  <Link to="/login">Login</Link>
+</li>
+<li>
+  <Link to="/signup">Signup</Link>
+</li>
+</div>
+
+export default Navbar;
