3f133507ea044b3a021223ac98202366bf5fb4a5
Roshni Daksha <groshnidaksha2@yahoo.com>
2024-02-03 14:42:27 +0800

    Interactive Navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -54,16 +54,4 @@ const Navbar = () => {
     );
 }
 
-<div>
-<li>
-  <Link to="/">Main</Link>
-</li>
-<li>
-  <Link to="/login">Login</Link>
-</li>
-<li>
-  <Link to="/signup">Signup</Link>
-</li>
-</div>
-
 export default Navbar;
